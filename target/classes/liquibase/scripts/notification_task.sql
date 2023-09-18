5c571bbd0ce3318cb8d01c6b07d7f0d79215fadd
vpm789 <vpm789@gmail.com>
2023-09-18 12:52:29 +0300

    tgBot_br1

--- target/classes/liquibase/scripts/notification_task.sql
+++ target/classes/liquibase/scripts/notification_task.sql
@@ -1,1 +1,8 @@
 --liquibase formatted sql
+create table notification_task
+(
+    id        serial    not null,
+    chat_id   serial    not null,
+    note_text text      not null,
+    date_time timestamp not null
+);
